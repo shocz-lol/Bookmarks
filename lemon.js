f6c431fc820fdd728b80280d610fd588e61416f4
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 21:20:22 -0500

    Update lemon.js

--- lemon.js
+++ lemon.js
@@ -1,2 +1,2 @@
 alert('Lemon Bot 0.1');
-lert("All the lemon bots do is fill your search history with lemon (just btw)");var a = prompt("How many lemon bots? ONLY ADD ABOUT 5-10","");for(var i=0; i<a;i++){window.open("https://lemonbot.shocz.repl.co","_blank")};
+alert("All the lemon bots do is fill your search history with lemon (just btw)");var a = prompt("How many lemon bots? ONLY ADD ABOUT 5-10","");for(var i=0; i<a;i++){window.open("https://lemonbot.shocz.repl.co","_blank")};
