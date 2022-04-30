2912052309af173bf71105cd8b9f19de335d87e2
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 22:29:23 -0500

    Update lemon2.js

--- lemon2.js
+++ lemon2.js
@@ -1,2 +1,2 @@
 alert('Lemon Bot 2 0.1');
-alert("This bot fills your search history with a speific amount. Type in a number and let them run. They will auto close when done.");var a = prompt("How many lemons in your search history? If you put too many it wont work fully. I recommed about 100-150 at a time.","");for(var i=0; i<a;i++){window.open("https://lemonbot2.shocz.repl.co#" + Math.random())};
+alert("This bot fills your search history with a speific amount. Type in a number and let them run. They will auto close when done.");var a = prompt("How many lemons in your search history? If you put too many it wont work fully. I recommed about 100-150 at a time.","");for(var i=0; i<a;i++){window.open("https://79e680d7-4b11-4ea3-a51e-162e26f60066.id.repl.co#" + Math.random())};
