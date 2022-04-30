bb7f8d22f64c9fd7816fffb018f879ecfd16ce1c
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 21:27:28 -0500

    Create embeder.js

---
+++ embeder.js
@@ -0,0 +1,2 @@
+alert('Embeder 0.1');
+!function(){var t=document.createElement("embed");t.style.height = "100%";t.style.width="100%";t.style.position = "absolute";t.style.zIndex = "999999";t.setAttribute("src","https://embeder.shocz.repl.co/"),document.body.appendChild(t)}();
