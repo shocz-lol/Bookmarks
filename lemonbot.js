4ddbb9269ca31ef85722f9712d1ac3de81913f20
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 22:30:40 -0500

    Create lemonbot.js

---
+++ lemonbot.js
@@ -0,0 +1,2 @@
+alert('Lemon Bot 0.1');
+alert("All the lemon bots do is fill your search history with lemon (just btw)");var a = prompt("How many lemon bots? ONLY ADD ABOUT 5-10","");for(var i=0; i<a;i++){window.open("https://9b84da7f-0aea-41cf-ae3f-897b9ad79f2c.id.repl.co","_blank")};
