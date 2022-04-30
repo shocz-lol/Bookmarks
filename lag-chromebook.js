590e45faebc5633528a120238ccc882a2b27fea8
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 21:32:39 -0500

    Create lag-chromebook.js

---
+++ lag-chromebook.js
@@ -0,0 +1,2 @@
+alert('Lag Chromebook 0.1');
+(function(){var a = confirm('Are you sure you want to run this. It could crash your device.'); if(a==true){while (true) {window.open(document.URL, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes')}}})()
