df7744083678157b26182b9c56774b31e5b21e09
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-30 18:49:46 -0500

    Update canvasdashboard.js

--- canvasdashboard.js
+++ canvasdashboard.js
@@ -1,2 +1,2 @@
 alert('Canvas 0.1');
-window.open("https://fc7b872d-bc70-4fde-9567-82b49b9974a0.id.repl.co/","_blank");
+window.open("https://fc7b872d-bc70-4fde-9567-82b49b9974a0.id.repl.co/proxy/https:/www.google.com/","_blank");
