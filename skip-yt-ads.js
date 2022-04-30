6f6098802346370210859a5e9b3038d38bd46b4b
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 21:30:41 -0500

    Create skip-yt-ads.js

---
+++ skip-yt-ads.js
@@ -0,0 +1,2 @@
+alert('Skip YT Ads 0.1');
+(function(){document.querySelector("video").currentTime = document.querySelector("video").duration; document.querySelector("ytp-ad-skip-button ytp-button").click()})();
