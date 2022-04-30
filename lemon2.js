2e6f6c4f6c7435a5632123115740d54c66acd774
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 21:23:52 -0500

    Create lemon2.js

---
+++ lemon2.js
@@ -0,0 +1,2 @@
+alert('Lemon Bot 2 0.1');
+alert("This bot fills your search history with a speific amount. Type in a number and let them run. They will auto close when done.");var a = prompt("How many lemons in your search history? If you put too many it wont work fully. I recommed about 100-150 at a time.","");for(var i=0; i<a;i++){window.open("https://lemonbot2.shocz.repl.co#" + Math.random())};
