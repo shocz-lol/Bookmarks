6581e293b024d398c05a38619242c7c03e92a9c9
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-04-29 22:35:10 -0500

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,2 @@
+alert('Embeder 0.1');
+!function(){var t=document.createElement("embed");t.style.height = "100%";t.style.width="100%";t.style.position = "absolute";t.style.zIndex = "999999";t.setAttribute("src","https://16821cd7-7c71-48c0-a542-401b62c0718e.id.repl.co"),document.body.appendChild(t)}();
