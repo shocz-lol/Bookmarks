b7c64f88439c42eccb9dc21f1b00cd7bd02144fc
shocz <76273033+shocz-lol@users.noreply.github.com>
2022-05-02 20:38:24 -0500

    Update lag-chromebook-v2.js

--- lag-chromebook-v2.js
+++ lag-chromebook-v2.js
@@ -1,1 +1,2 @@
-(function(){alert('Lag Chromebook V2');var a = confirm(%27Are you sure you want to run this? It could crash your device.%27); if(a==true){window.open('https://02377b2e-9cc2-4568-9f57-3442b3c1d626.id.repl.co/','_blank')}})()
+alert('Lag Chromebook V2');
+(function(){var a = confirm(%27Are you sure you want to run this? It could crash your device.%27); if(a==true){window.open('https://02377b2e-9cc2-4568-9f57-3442b3c1d626.id.repl.co/','_blank')}})()
